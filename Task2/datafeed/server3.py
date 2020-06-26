9d41d2b14ad49d8b4e8922aff53460fe2a27137b
ManojBogandham <manojbvs01@gmail.com>
2020-06-26 17:31:56 +0530

    test.csv created

--- Task2/datafeed/server3.py
+++ Task2/datafeed/server3.py
@@ -241,7 +241,7 @@ class App(object):
         self._data_1    = order_book(read_csv(), self._book_1, 'ABC')
         self._data_2    = order_book(read_csv(), self._book_2, 'DEF')
         self._rt_start = datetime.now()
-        self._sim_start, _, _  = next(self._data_1)
+        self._sim_start,_,_ = next(self._data_1)
         self.read_10_first_lines()
 
     @property
