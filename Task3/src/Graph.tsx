64029f654569999bff8c55e07cbdf850b2606138
Manoj Boganadham <manojbvs01@gmail.com>
2020-07-02 16:25:50 +0530

    task3

--- Task3/src/Graph.tsx
+++ Task3/src/Graph.tsx
@@ -23,13 +23,10 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      price_abc: 'float',
-      price_def: 'float',
-      ratio: 'float',
+      stock: 'string',
+      top_ask_price: 'float',
+      top_bid_price: 'float',
       timestamp: 'date',
-      upper_bound:'float',
-      lower_bound: 'float',
-      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -39,25 +36,23 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
       elem.setAttribute('aggregates', JSON.stringify({
-      price_abc: 'avg',
-      price_def: 'avg',
-      ratio: 'avg',
-      timestamp: 'distinct_count',
-      upper_bound:'avg',
-      lower_bound: 'avg',
-      trigger_alert: 'avg',
+        stock: 'distinctcount',
+        top_ask_price: 'avg',
+        top_bid_price: 'avg',
+        timestamp: 'distinct count',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update([
+      this.table.update(
         DataManipulator.generateRow(this.props.data),
-      ]);
+      );
     }
   }
 }
